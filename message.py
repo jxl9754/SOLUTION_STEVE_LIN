0de4da2e0b6bba9a1d569f2db38d704ff64876b1
stevelin <stevelin@exosfinancial.com>
2019-04-03 13:10:59 -0400

    Modify abort exception

--- message.py
+++ message.py
@@ -17,12 +17,13 @@ def get_message(hash_msg):
     :param hash_msg:   hash of message to find
     :return:        message and 200 return code or 404 on hash not found
     """
-    # Does the hash exist in hsah_message?
+    # Does the hash exist in hash_message - status 404
     if hash_msg in hash_message:
         message_key = hash_message.get(hash_msg)
         return {"message": message_key}, 200
-    # otherwise, nope, not found
+    # otherwise, not found abort exception
     else:
+        # return {"err_msg": "Hash Message not found - " + hash_msg}, 404
         abort(404, {"err_msg": "Hash Message not found - " + hash_msg})
 
 
@@ -35,11 +36,12 @@ def create_hash_sha256(msg):
     """
     message_key = msg.get("message", None)
 
-    # Does the person exist already?
+    # Does the input message exist?
     if message_key is not None:
         hash_object=hashlib.sha256(message_key.encode('utf-8'))
         hash_message[hash_object.hexdigest()] = message_key
         return {"digest": hash_object.hexdigest()}, 201
-    # Otherwise, they exist, that's an error
+    # Otherwise, that's an abort error - status 404
     else:
+        # return {"err_msg": "No input Message"}, 404
         abort(404, {"err_msg": "No input Message"})
